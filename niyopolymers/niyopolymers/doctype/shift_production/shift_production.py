ded0c1450b3fc4840e1961fb3f06099e42fe1f36
venkatesh.nayak <venkatesh.nayak@atriina.com>
2021-05-31 17:46:50 +0530

    Shift Production doctype code copied to niyoploymers

--- niyopolymers/niyopolymers/doctype/shift_production/shift_production.py
+++ niyopolymers/niyopolymers/doctype/shift_production/shift_production.py
@@ -8,3 +8,22 @@ from frappe.model.document import Document
 
 class ShiftProduction(Document):
 	pass
+
+@frappe.whitelist()
+def create_stock_entry(doc, handler=""):
+    se = frappe.new_doc("Stock Entry")
+    se.update({ "purpose": "Manufacture" , "stock_entry_type": "Manufacture"})
+    for se_item in doc.items:
+        se.append("items", { "item_code":se_item.preform, "qty": se_item.blowing, "s_warehouse": "Stores - NP"})
+    for se_item in doc.items:
+        se.append("items", { "item_code":se_item.product_code, "qty": se_item.blowing_perform, "t_warehouse":"Finished Goods - NP"})
+    for se_item in doc.items:
+        se.append("items", { "item_code":se_item.rejection_item, "qty": se_item.rejection, "t_warehouse": "Scrap Warehouse - NP"})
+    for se_item in doc.items:
+        se.append("items", { "item_code":se_item.rejection_white, "qty": se_item.white, "t_warehouse": "Scrap Warehouse - NP"})
+    for se_item in doc.items:
+        se.append("items", { "item_code":se_item.rejection_hot, "qty": se_item.hot, "t_warehouse": "Scrap Warehouse - NP"})
+
+#    frappe.msgprint('Stock Entry is created please submit the stock entry')
+        se.docstatus=1
+        se.insert()
