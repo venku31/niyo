0b1004a58a2a1af09b2f47ab30f0bfb65889d5f7
halimasayyed <halima.sayyed@atriina.com>
2021-06-19 13:51:52 +0530

    create attendance on holidays

--- niyopolymers/overrides/attendance_request.py
+++ niyopolymers/overrides/attendance_request.py
@@ -14,7 +14,7 @@ def create_attendance(self):
     request_days = date_diff(self.to_date, self.from_date) + 1
     for number in range(request_days):
         attendance_date = add_days(self.from_date, number)
-        skip_attendance = self.validate_if_attendance_not_applicable(attendance_date)
+        skip_attendance = validate_if_attendance_not_applicable(self, attendance_date)
         if not skip_attendance:
             attendance = frappe.new_doc("Attendance")
             attendance.employee = self.employee
@@ -31,5 +31,16 @@ def create_attendance(self):
             attendance.save(ignore_permissions=True)
             attendance.submit()
 
+def validate_if_attendance_not_applicable(self, attendance_date):
+		# Check if employee on Leave
+		leave_record = frappe.db.sql("""select half_day from `tabLeave Application`
+			where employee = %s and %s between from_date and to_date
+			and docstatus = 1""", (self.employee, attendance_date), as_dict=True)
+		if leave_record:
+			frappe.msgprint(_("Attendance not submitted for {0} as {1} on leave.").format(attendance_date, self.employee), alert=1)
+			return True
+
+		return False
 
 AttendanceRequest.create_attendance = create_attendance
+AttendanceRequest.validate_if_attendance_not_applicable = validate_if_attendance_not_applicable
