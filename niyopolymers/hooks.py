3af9cdf7f9b971c6fcb69a60af55a7ec1d7a9a84
halimasayyed <halima.sayyed@atriina.com>
2021-06-24 17:17:15 +0530

    send probation period end notification

--- niyopolymers/hooks.py
+++ niyopolymers/hooks.py
@@ -236,7 +236,8 @@ scheduler_events = {
 		"niyopolymers.hr.send_mail_to_employees_on_shift_end"
     ],
 	"daily": [
-        "niyopolymers.hr.maternity_leave_mail"
+        "niyopolymers.hr.maternity_leave_mail",
+		"niyopolymers.hr.send_probation_peroid_end_notification"
     ]
 }
 
