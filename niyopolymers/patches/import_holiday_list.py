6bc66ae5da2c673af8f16f6c6e7b1396f14027fb
halimasayyed <halima.sayyed@atriina.com>
2021-07-07 18:06:56 +0530

    fix holiday

--- niyopolymers/patches/import_holiday_list.py
+++ niyopolymers/patches/import_holiday_list.py
@@ -2,4 +2,4 @@ import frappe
 
 def execute():
     path = frappe.get_app_path("niyopolymers", "patches", "imports", "holiday_list.csv")
-    frappe.core.doctype.data_import.data_import.import_file("Holiday", path, "Insert", console=True)
+    frappe.core.doctype.data_import.data_import.import_file("Holiday List", path, "Insert", console=True)
