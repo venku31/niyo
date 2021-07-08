03ff4e6d1ee2dd6a58bbcd8ffd46e48413725d6c
halimasayyed <halima.sayyed@atriina.com>
2021-07-08 15:17:28 +0530

    import warning letter template using patches

--- niyopolymers/patches/import_bom.py
+++
@@ -1,5 +0,0 @@
-import frappe
-
-def execute():
-    path = frappe.get_app_path("niyopolymers", "patches", "imports", "bom.csv")
-    frappe.core.doctype.data_import.data_import.import_file("BOM", path, "Insert", console=True)
