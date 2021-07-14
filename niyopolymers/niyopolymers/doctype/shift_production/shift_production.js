029e24619692d5754ebb57f55049e7aa79c13836
venkatesh.nayak@atriina.com <venkatesh.nayak@atriina.com>
2021-07-14 17:58:24 +0530

    Shift Production Stock Entry Valuation,remarks,cost

--- niyopolymers/niyopolymers/doctype/shift_production/shift_production.js
+++ niyopolymers/niyopolymers/doctype/shift_production/shift_production.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Atriina and contributors
+/ Copyright (c) 2021, Atriina and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shift Production', {
@@ -13,6 +13,24 @@ cur_frm.fields_dict.supervisor.get_query = function(doc) {
 			}
  	}
  }
+ frappe.ui.form.on("Shift Production Details", "preform", function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	return frappe.call({
+		method:"erpnext.stock.utils.get_incoming_rate", 
+		args: {
+			args: {
+				"item_code": d.preform, 
+				"warehouse": d.rm_warehouse
+			}
+		},
+		callback: function(r) {
+			if (r.message) {
+				frappe.model.set_value(cdt, cdn, "basic_rate", r.message);
+			}
+		}
+	});
+});
+
 frappe.ui.form.on('Shift Production Details', {
 	blowing:function(frm,cdt,cdn){
 		var d = locals[cdt][cdn];
@@ -58,3 +76,19 @@ frappe.ui.form.on('Shift Production Details', {
 		frappe.model.set_value(cdt, cdn, "blowing_perform", blowing_perform);	
 	}
 })
+frappe.ui.form.on("Shift Production", {
+	validate: function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var total_qty = 0;
+	frm.doc.items.forEach(function(d) { total_qty += d.blowing; });
+			frm.set_value('total_qty', total_qty);
+	}
+	});
+	
+	
+	
+	frappe.ui.form.on('Shift Production', {
+	validate(frm) {
+	cur_frm.doc.total_cost=cur_frm.doc.cost_per_preform*cur_frm.doc.total_qty;
+	}
+	})
